0af1d0674ada5c39ee96a112514d24515b8de28c
shunichi <shunichi@hiraiwashunichinoMacBook-Air.local>
2017-03-08 09:14:25 +0900

    Initial Commit

---
+++ StopWatchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopWatchApp
+//
+//  Created by 平岩駿一 on 2017/03/08.
+//  Copyright © 2017年 shunichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
